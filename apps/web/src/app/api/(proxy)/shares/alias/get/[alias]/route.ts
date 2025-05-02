5b3dab7c75ae01bb8ab6a2fadce4f514a165aff8
PunchEnergyFTW <61878903+PunchEnergyFTW@users.noreply.github.com>
2025-05-02 22:01:22 +0200

    fix(shares): Allow users to set and update passwords for shares (#25)

--- apps/web/src/app/api/(proxy)/shares/alias/get/[alias]/route.ts
+++ apps/web/src/app/api/(proxy)/shares/alias/get/[alias]/route.ts
@@ -2,16 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, { params }: { params: { alias: string } }) {
   const cookieHeader = req.headers.get("cookie");
-  const queryParams = new URLSearchParams(req.url.split("?")[1]) || undefined;
-
-  const apiRes = await fetch(`${process.env.API_BASE_URL}/shares/alias/${params.alias}`, {
+  const url = new URL(req.url);
+  const queryParams = url.search;
+  const apiRes = await fetch(`${process.env.API_BASE_URL}/shares/alias/${params.alias}${queryParams}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       cookie: cookieHeader || "",
     },
     redirect: "manual",
-    ...(queryParams ? { params: queryParams } : {}),
   });
 
   const resBody = await apiRes.text();
